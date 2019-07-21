17289f893214a1ec99fbb83719a8e5bbdc467ed3
Veiko <veiko.aunapuu@gmail.com>
2019-07-21 15:36:02 +0300

    JS file changes

--- ColorPicker.js
+++ ColorPicker.js
@@ -7,7 +7,24 @@ var colors = [
     "rgb(255, 255, 255)"
 ]
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
+    //add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
+
+    //add click listeners to squares
+    squares[i].addEventListener("click",function(){
+        //grab color of clicked square
+        var clickedColor = this.style.background;
+        //compare color to pickedColor
+        if(clickedColor === pickedColor) {
+            alert("Correct!");
+        } else {
+            this.style.background = "rgb(0, 0, 0)";
+        }
+
+    })
 }
